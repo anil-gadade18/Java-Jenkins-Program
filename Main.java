ff6907fa00b9650ccd74cbbe15d32246214c36f5
anil-gadade18 <anilgadade2021@gmail.com>
2025-03-20 21:50:14 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+        public static void main(String[] args) {
+            
+            System.out.println("Learing Jenkins for CI/CD");
+            System.out.println("Welcome to Jenkins Program!!");
+        }
+
+}
